12fead557b107cd53412c712911d5c6176a54bdb
bailu1901 <blue-1986@hotmail.com>
2014-04-17 22:14:01 +0800

    mainmenu_scene

--- project/main.js
+++ project/main.js
@@ -3,8 +3,8 @@ cc.game.onStart = function(){
 	cc.view.resizeWithBrowserSize(true);
     //load resources
     cc.LoaderScene.preload(g_resources, function () {
-        //cc.director.runScene(new MainMenuScene());
-        cc.director.runScene(new GameScene());
+        cc.director.runScene(new MainMenuScene());
+        //cc.director.runScene(new GameScene());
     }, this);
 };
 cc.game.run();
